3733fb7ae06f148f5f241e13a89cb03106921279
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-11-07 08:59:04 +0100

    ADD possibilities to save a password in a csv file

--- pass_generator.py
+++ pass_generator.py
@@ -1,5 +1,6 @@
-import string
+import string 
 import random
+from csv import writer # pour écrire dans un fichier csv
 
 def generate_pass():
     s1 = string.ascii_lowercase # Crée une liste de caractères minuscules
@@ -7,7 +8,8 @@ def generate_pass():
     s3 = string.digits
     s4 = string.punctuation
 
-    pass_length = int(input("Enter the length of the s: \n"))
+    platform = input("Enter the platform: \n") # Demande à l'utilisateur de saisir la plateforme où le mot de passe sera utilisé
+    pass_length = int(input("Enter the length of the password: \n"))
 
     s = [] # variable pour stocker le mot de passe
     s.extend(list(s1)) # Ajoute tous les caractères de s1 à s
@@ -19,5 +21,10 @@ def generate_pass():
     password = ("".join(s[0:pass_length])) # joint les caractères de la liste pour former un mot de passe
     
     print(password)
+
+    passdata = [platform, password] # Stocke la plateforme et le mot de passe dans une liste
+    with open('pass.csv', 'a') as f: # Crée un fichier csv
+        writedata = writer(f) # Écrit dans le fichier csv
+        writedata.writerow(passdata) # Écrit les données dans le fichier csv
 
 generate_pass()
